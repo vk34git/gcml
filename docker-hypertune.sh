a7963b63aac04f6b4816909ce6013e5aa5eb8b90
vas <vk34@yahoo.com>
2023-01-12 18:52:25 +0000

    initial

--- docker-hypertune.sh
+++ docker-hypertune.sh
@@ -2,3 +2,8 @@ export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 IMAGE_URI="gcr.io/$PROJECT_ID/horse-human:hypertune"
 docker build ./ -t $IMAGE_URI
 docker push $IMAGE_URI
+git add .
+git config --global user.email "vk34@yahoo.com"
+git config --global user.name "vas"
+git config --global user.password "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"
+git commit -m "initial" .
