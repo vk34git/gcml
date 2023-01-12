f8d9a56aec0b0aa146b8e7483b6c7a66de4e7970
vas <vk34@yahoo.com>
2023-01-12 18:56:32 +0000

    initial

--- docker-hypertune.sh
+++ docker-hypertune.sh
@@ -1,9 +1,12 @@
+git add remote gcml-remote https://github.com/vk34git/gcml
+git config --global user.email "vk34@yahoo.com"
+git config --global user.name "vas"
+git config --global user.password "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"
+git pull
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
 IMAGE_URI="gcr.io/$PROJECT_ID/horse-human:hypertune"
 docker build ./ -t $IMAGE_URI
 docker push $IMAGE_URI
 git add .
-git config --global user.email "vk34@yahoo.com"
-git config --global user.name "vas"
-git config --global user.password "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"
 git commit -m "initial" .
+git push gcml-remote master 
